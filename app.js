14164372710af771715347b124fa24ab19a0f11a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-01 17:53:31 -0400

    Added a function to display the data returned from the text file.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ document.querySelector('#btn-1').addEventListener('click', getText);
 
 function getText() {
   fetch('test.txt').then(function(response) {
-    return response.text().then(function(data) {});
+    return response.text().then(function(data) {
+      console.log(data);
+    });
   });
 }
