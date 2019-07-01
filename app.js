273f19b192d41f2a766e36a966b22a06021ba2e2
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-01 17:57:56 -0400

    Added a catch to catch any errors.

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ document.querySelector('#btn-1').addEventListener('click', getText);
 
 function getText() {
   fetch('test.txt').then(function(response) {
-    return response.text().then(function(data) {
-      console.log(data);
-    });
+    return response
+      .text()
+      .then(function(data) {
+        console.log(data);
+      })
+      .catch(function(error) {
+        console.error(error);
+      });
   });
 }
