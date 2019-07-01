02f6ff5858e537b7d61f69b3dd0504f93008511f
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-01 17:52:13 -0400

    Added the next then.

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ document.querySelector('#btn-1').addEventListener('click', getText);
 
 function getText() {
   fetch('test.txt').then(function(response) {
-    return response.text();
+    return response.text().then(function(data) {});
   });
 }
